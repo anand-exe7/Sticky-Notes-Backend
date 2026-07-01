b8d49bda1f6d397ee4fe4041b3b6000a9e41406c
anand-exe7 <anandsiva0317@gmail.com>
2026-07-01 21:48:17 +0530

    forgot to push types.go

---
+++ internal/model/types.go
@@ -0,0 +1,30 @@
+package model
+
+import "time"
+
+type NoteColor string
+
+const (
+	ColorYellow NoteColor = "yellow"
+	ColorBlue   NoteColor = "blue"
+	ColorGreen  NoteColor = "green"
+	ColorPink   NoteColor = "pink"
+	ColorOrange NoteColor = "orange"
+)
+
+type StickyNote struct {
+	ID        string    `json:"id" bson:"_id,omitempty"`
+	Title     string    `json:"title" bson:"title"`
+	Content   string    `json:"content" bson:"content"`
+	Color     NoteColor `json:"color" bson:"color"`
+	Pinned    bool      `json:"pinned" bson:"pinned"`
+	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
+}
+
+type NoteFormData struct {
+	Title   string    `json:"title" bson:"title"`
+	Content string    `json:"content" bson:"content"`
+	Color   NoteColor `json:"color" bson:"color"`
+}
+

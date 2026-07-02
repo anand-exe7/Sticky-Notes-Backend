aff536b2af4a69a6fa74b8229af26892636cd55f
anand-exe7 <anandsiva0317@gmail.com>
2026-07-02 10:01:39 +0530

    defined handler interfaces for note operations

---
+++ internal/handler/types.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"context"
+	"time"
+	"sticky-notes-go-backend/internal/model"
+)
+
+type NoteStorer interface {
+
+	CreateNotes(ctx context.Context, m model.StickyNote) (*model.StickyNote, error)
+
+	GetNoteById(ctx context.Context, id string) (*model.StickyNote, error)
+	
+	GettALLNotes(ctx context.Context) ([]*model.StickyNote, error)
+	
+	EditNote(ctx context.Context, id string, data model.StickyNote) (model.StickyNote, error)
+	
+	DeleteNote(ctx context.Context, id string) error
+}
+
+type NoteRequest struct {
+	Title   string          `json:"title"`
+	Content string          `json:"content"`
+	Color   model.NoteColor `json:"color"`
+	Pinned  bool            `json:"pinned"`
+}
+
+type NoteResponse struct {
+	ID        string          `json:"id"`
+	Title     string          `json:"title"`
+	Content   string          `json:"content"`
+	Color     model.NoteColor `json:"color"`
+	Pinned    bool            `json:"pinned"`
+	CreatedAt time.Time       `json:"createdAt"`
+	UpdatedAt time.Time       `json:"updatedAt"`
+}

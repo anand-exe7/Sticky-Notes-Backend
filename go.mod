e00d73461614d3a97767419a431a9aa81063fb38
anand-exe7 <anandsiva0317@gmail.com>
2026-07-02 22:51:15 +0530

    added CORS middleware for note API endpoints to talk with the frontend

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.26.2
 
 require (
 	github.com/go-chi/chi/v5 v5.3.0
+	github.com/go-chi/cors v1.2.2
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver/v2 v2.7.0
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func NewRouter(h *handler.Handler) *chi.Mux {
@@ -13,11 +14,20 @@ func NewRouter(h *handler.Handler) *chi.Mux {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
 	r.Route("/api/notes", func(r chi.Router) {
 		
 		r.Post("/", h.CreateNote)
 		r.Get("/", h.GetAllNotes)
-		r.Get("/trash", h.GetTrashNotes)
+		r.Get("/trash", h.GetTrashNotes) 
 
 		r.Get("/{id}", h.GetNoteByID)
 		r.Put("/{id}", h.EditNote)
@@ -25,6 +35,7 @@ func NewRouter(h *handler.Handler) *chi.Mux {
 
 		r.Post("/{id}/restore", h.RestoreNote)
 		r.Delete("/{id}/permanent", h.PermanentDeleteNote)
+		r.Patch("/{id}/pin", h.TogglePinNote)
 	})
 
 	return r
